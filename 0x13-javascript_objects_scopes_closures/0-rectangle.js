081a36eac9032601568bd4c1016f9af3ae443a4e
MO3 <mrinfo2002@gmail.com>
2024-06-19 11:41:51 +0000

    0-rectangle.jsss

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,5 +1,5 @@
-#!/usr/bin/node
-class Rectangle {
-}
+const Rectangle = require('./0-rectangle');
 
-module.exports = Rectangle;
+const r1 = new Rectangle();
+console.log(r1);
+console.log(r1.constructor); 
